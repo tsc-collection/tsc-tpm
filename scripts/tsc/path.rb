2d4f4f8e414544dcbee12825c87babe085edffea
bystr <bystr@af914a7b-af0d-0410-90d3-cea0664cafdb>
2006-10-05 05:30:46 +0000

    Added forgotten copyright notice.
    git-svn-id: https://svn.code.sf.net/p/tsc-tpm/code@65 af914a7b-af0d-0410-90d3-cea0664cafdb

--- scripts/tsc/path.rb
+++ scripts/tsc/path.rb
@@ -1,3 +1,8 @@
+# Copyright (c) 2006, Gennady Bystritsky <bystr@mac.com>
+# 
+# Distributed under the MIT Licence.
+# This is free software. See 'LICENSE' for details.
+# You must read and accept the license prior to use.
 
 require 'tsc/errors.rb'
 
