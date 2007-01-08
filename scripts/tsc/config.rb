24f1567785ed68bfd575b060435799dc5beba51d
bystr <bystr@af914a7b-af0d-0410-90d3-cea0664cafdb>
2007-01-08 22:44:38 +0000

    Added a dummy test to pass other suites. Otherwise it fails on "no default test".
    git-svn-id: https://svn.code.sf.net/p/tsc-tpm/code@102 af914a7b-af0d-0410-90d3-cea0664cafdb

--- scripts/tsc/config.rb
+++ scripts/tsc/config.rb
@@ -88,6 +88,9 @@ if $0 == __FILE__ or defined?(Test::Unit::TestCase)
   
   module TSC
     class ConfigTest < Test::Unit::TestCase
+      def test_nothing
+      end
+
       def setup
       end
       
