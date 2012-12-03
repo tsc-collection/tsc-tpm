8c89f5bcb6c5718fc69e4b0973ed7a4b704b518e
bystr <bystr@af914a7b-af0d-0410-90d3-cea0664cafdb>
2012-12-03 03:01:28 +0000

    Using single quotes instead of double.
    git-svn-id: https://svn.code.sf.net/p/tsc-tpm/code@446 af914a7b-af0d-0410-90d3-cea0664cafdb

--- scripts/tsc/unit-tests/application-spec.rb
+++ scripts/tsc/unit-tests/application-spec.rb
@@ -26,22 +26,22 @@ describe TSC::Application do
     }
   end
 
-  context "options" do
-    it "should have correct display format" do
+  context 'options' do
+    it 'should have correct display format' do
       app.registry.format_entries.should == [
-        ["           -v, ", "--verbose      ", "Turns verbose mode on"],
-        ["       -h, -?, ", "--help         ", "Prints out this help message"],
-        ["               ", "--debug        ", "Starts the interactive debugger"],
-        ["       -a, -A, ", "--action <name>", "Action"],
-        ["-m, --machine, ", "--host <name>  ", "Host"],
-        ["               ", "--system <name>", "System"],
-        ["           -f, ", "--force        ", "Force"],
-        ["               ", "--quiet        ", "Quiet"]
+        ['           -v, ', '--verbose      ', 'Turns verbose mode on'],
+        ['       -h, -?, ', '--help         ', 'Prints out this help message'],
+        ['               ', '--debug        ', 'Starts the interactive debugger'],
+        ['       -a, -A, ', '--action <name>', 'Action'],
+        ['-m, --machine, ', '--host <name>  ', 'Host'],
+        ['               ', '--system <name>', 'System'],
+        ['           -f, ', '--force        ', 'Force'],
+        ['               ', '--quiet        ', 'Quiet']
       ]
     end
   end
 
-  context "option processing" do
+  context 'option processing' do
     before do
       ARGV.replace [ '-v', '-a', 'zzz', '-a', 'bbb', '-m', '', '-fff' ]
       @options = app.start { |_app|
@@ -53,37 +53,37 @@ describe TSC::Application do
       ARGV.replace []
     end
 
-    it "should have proper option with multiple arguments" do
+    it 'should have proper option with multiple arguments' do
       options.action?.should == true
       options.action.should == 'zzz'
       options.action_list.should == [ 'zzz', 'bbb' ]
     end
 
-    it "should not have not specified argument entry" do
+    it 'should not have not specified argument entry' do
       options.system?.should == false
       options.system.should == nil
       options.system_list.should == []
     end
 
-    it "should not have not specified predicate entry" do
+    it 'should not have not specified predicate entry' do
       options.quiet?.should == false
       options.quiet.should == nil
     end
 
-    it "should have proper option with empty string argument" do
+    it 'should have proper option with empty string argument' do
       options.host?.should == true
       options.host.should == ''
       options.host_list.should == [ '' ]
     end
 
-    it "should have proper single and multiple predicate options" do
+    it 'should have proper single and multiple predicate options' do
       options.verbose?.should == true
       options.verbose.should == 1
       options.force.should == 3
     end
   end
 
-  context "verbose option processing" do
+  context 'verbose option processing' do
     before do
       ARGV.replace [ '-v', '-v' ]
       @options = app.start { |_app|
@@ -95,18 +95,18 @@ describe TSC::Application do
       ARGV.replace []
     end
 
-    it "should have proper option count" do
+    it 'should have proper option count' do
       options.verbose?.should == true
       options.verbose.should == 2
     end
 
-    it "should be possible to reset it to false" do
+    it 'should be possible to reset it to false' do
       options.verbose = false
       options.verbose?.should == false
       options.verbose.should == nil
     end
 
-    it "should be possible to reset it to true" do
+    it 'should be possible to reset it to true' do
       options.verbose = true
       options.verbose?.should == true
       options.verbose.should == 2
@@ -120,25 +120,25 @@ describe TSC::Application do
       options.verbose.should == 1
     end
 
-    it "should be possible to reset it to a positive count" do
+    it 'should be possible to reset it to a positive count' do
       options.verbose = 5
       options.verbose?.should == true
       options.verbose.should == 5
     end
 
-    it "should be possible to reset it to zero" do
+    it 'should be possible to reset it to zero' do
       options.verbose = 0
       options.verbose?.should == false
       options.verbose.should == nil
     end
 
-    it "should treat gabberish as false" do
-      options.verbose = "abc"
+    it 'should treat gabberish as false' do
+      options.verbose = 'abc'
       options.verbose?.should == false
       options.verbose.should == nil
     end
 
-    it "should be available via application" do
+    it 'should be available via application' do
       app.verbose?.should == true
       app.verbose.should == 2
     end
